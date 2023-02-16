952d913776e18545b3eeb1b5dbaf5b1e859b5ceb
Raouya El yaagoubi <raouya_el@hotmail.com>
2023-02-16 17:05:30 +0100

    first commits

---
+++ src/main/java/com/example/userbankly/UserBanklyApplication.java
@@ -0,0 +1,13 @@
+package com.example.userbankly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBanklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBanklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userbankly/UserBanklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userbankly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBanklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
